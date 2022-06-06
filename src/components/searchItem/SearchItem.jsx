6d6b529da7c01756b76e86efff7212eaa86c73b4
Simran Shah <simransah2401@gmail.com>
2022-06-06 12:40:08 +0530

    edited

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -4,7 +4,12 @@ import './searchItem.css';
 const SearchItem = ({ item }) => {
   return (
     <div className='searchItem'>
-      <img src={item.photos[0]} alt='' className='siImg' />
+      {/* <img src={item.photos[0]} alt='' className='siImg' /> */}
+      <img
+        src='https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1'
+        alt=''
+        className='siImg'
+      />
       <div className='siDesc'>
         <h1 className='siTitle'>{item.name}</h1>
         <span className='siDistance'>{item.distance}m from center</span>
