3fa9b3013ffebc9a5d082c54e20f3b9075541d4c
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-06-19 13:46:29 +0530

    fixed bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -43,6 +43,8 @@ const Footer = () => {
           <li className='fListItem'>Investor relations</li>
           <li className='fListItem'>Terms & conditions</li>
         </ul>
+      </div>
+    </div>
 
   );
 };
